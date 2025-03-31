3b9f1df683b0914c73e0ea87c9547967d89d3d87
DEEPAK MAURYA <deepak965147@gmail.com>
2025-03-31 18:41:37 +0530

    Add files via upload

--- star.java
+++ star.java
@@ -1,5 +1,11 @@
+
+import javax.lang.model.element.VariableElement;
+
+//1 Star
+
 public class Star {
     public static void main(String[] args) {
+
         System.out.println("*");
         System.out.println("* *");
         System.out.println("* * *");
@@ -7,6 +13,35 @@ public class Star {
         System.out.println("* * * * *");
         System.out.println("* * * * * *");
         System.out.println("* * * * * * *");
+        //Variables
+//2 Simpal Calculator Define by Naumber sum, diff,mul,ans,pus;
+    int a = 10;
+    int b = 20;
+    int sum = a+b;
+    System.out.println(sum);
     
+    int diff = a-b;
+    System.out.println(diff);
+    
+    int mul = sum / diff;
+    System.out.println(mul);
+
+    int ans = sum*diff;
+    System.out.println(ans);
+
+    int pus = sum%diff;
+    System.out.println(pus);
+
+
+    
+
+//output;
+
+//30
+//-10
+//-3
+//-300
+//0
+
     }
 }
