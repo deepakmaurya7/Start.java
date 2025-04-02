7c2f927d3c970e06c7936b6a5ef2e28ef059fc6d
DEEPAK MAURYA <deepak965147@gmail.com>
2025-04-02 22:59:02 +0530

    Add files via upload

---
+++ evan.java
@@ -0,0 +1,18 @@
+//Define by Odd Number And Evan Number.
+import java.awt.desktop.SystemSleepEvent;
+import java.util.*;
+
+public class evan { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x =sc.nextInt();
+
+        if (x % 2 == 0){
+            System.out.print("Evan");
+        } else {
+
+            System.out.println(" Odd");
+        } 
+
+    }
+}
