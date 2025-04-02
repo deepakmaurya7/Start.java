948980e4252cf854d05b8393c95a4175bb0d3055
DEEPAK MAURYA <deepak965147@gmail.com>
2025-04-02 22:37:07 +0530

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,18 @@
+//Define by Age And Condition and Check Adulat or NOt Adult.
+import java.awt.desktop.SystemSleepEvent;
+import java.util.*;
+
+public class Condition { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age =sc.nextInt();
+
+        if (age>18){
+            System.out.print("Adult");
+        } else {
+
+            System.out.println("Not Adult");
+        } 
+
+    }
+}
