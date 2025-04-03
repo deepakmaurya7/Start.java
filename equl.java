1d0e5106311fbcf9bcfe3664fd3ce7584138c7d7
DEEPAK MAURYA <deepak965147@gmail.com>
2025-04-03 22:05:01 +0530

    Add files via upload

---
+++ equl.java
@@ -0,0 +1,25 @@
+//Define by Odd Number And Evan Number.
+import java.awt.desktop.SystemSleepEvent;
+import java.util.*;
+
+public class equl { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+
+        if (a  == b){
+            System.out.print("equal");
+        } else {
+            if (a >b ){
+                System.out.println(" a is Greater");
+            } else {
+                System.out.print("a is lesser");
+
+            }
+
+
+        }
+
+    }
+}
