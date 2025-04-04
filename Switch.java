7d9ed3c58027cd80e4b0b0ae9119c005dd027b66
DEEPAK MAURYA <deepak965147@gmail.com>
2025-04-04 22:19:35 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,41 @@
+// define  the Swituch Button
+import com.sun.source.tree.SwitchTree;
+import java.awt.desktop.SystemSleepEvent;
+import java.util.*;
+
+public class Switch { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Button =sc.nextInt();
+
+       // if (Button == 1){
+         //   System.out.print("Hello");
+        //} else if (Button == 2) {
+        //        System.out.println(" Namaste");
+        //} else if (Button == 3) {
+        //    System.out.println(" Bonjour ");
+        //} else if (Button == 4) {
+        //        System.out.println(" Hello deepak ");
+        //    } else {   
+        //        System.out.println(" Invalid Button ");
+        //    }
+
+
+        switch(Button){
+            case 1 : System.out.print("Hello");
+            break;
+            case 2 : System.out.print("Namest");
+            break;
+            case 3 : System.out.print("School");
+            break;
+            case 4 : System.out.print("Waiting");
+        }
+
+
+
+
+
+
+
+        }
+    }
