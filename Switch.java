32051976c6428ac09b0ff814bd18613080908307
DEEPAK MAURYA <deepak965147@gmail.com>
2025-04-04 22:20:53 +0530

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -6,22 +6,8 @@ import java.util.*;
 public class Switch { 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int Button =sc.nextInt();
-
-       // if (Button == 1){
-         //   System.out.print("Hello");
-        //} else if (Button == 2) {
-        //        System.out.println(" Namaste");
-        //} else if (Button == 3) {
-        //    System.out.println(" Bonjour ");
-        //} else if (Button == 4) {
-        //        System.out.println(" Hello deepak ");
-        //    } else {   
-        //        System.out.println(" Invalid Button ");
-        //    }
-
-
-        switch(Button){
+        int Button =sc.nextInt()
+            switch(Button){
             case 1 : System.out.print("Hello");
             break;
             case 2 : System.out.print("Namest");
