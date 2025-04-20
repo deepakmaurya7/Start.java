f85edae8469a370241bd81b3c8291cfab99be84d
DEEPAK MAURYA <deepak965147@gmail.com>
2025-04-20 23:33:44 +0530

    Update Bazinga.java

--- Bazinga.java
+++ Bazinga.java
@@ -1,5 +1,4 @@
 // define by Bazinga
-import java.awt.desktop.SystemSleepEvent;
 import java.util.*;
 
 public class Bazinga { 
@@ -10,11 +9,17 @@ public class Bazinga {
         if (Button == 1){
             System.out.print("Hello");
         } else if (Button == 2){
-                System.out.println(" Namaste");
-        } else if (Button == 3) {
-                System.out.println("Bonjour");
-            } else {
-                System.out.println("Invalid Button");
+                System.out.println(" My name is deepak maurya");
+        } else if (Button == 3){
+            System.out.println(" I am 20 Year old");
+        } else if (Button == 4) {
+                System.out.println("I am frome chhanhi sarnath varanasi");
+        } else if (Button == 5){
+                System.out.println(" I am study Computer Science");
+        } else if (Button == 6){
+            System.out.println("My Hubbies plying crekit and Wchting moves ");
+        }else {
+            System.out.println("Involid Button");
         }
     }
 }
