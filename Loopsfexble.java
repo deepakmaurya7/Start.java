9ee257233e6e7396459d3d3ddef79cf46f100b0b
DEEPAK MAURYA <deepak965147@gmail.com>
2025-04-22 22:53:42 +0530

    Add files via upload

---
+++ Loopsfexble.java
@@ -0,0 +1,8 @@
+// fex loops note infinate 1 to any tims print hi deepak maurya
+public class Loopsfexble  { 
+    public static void main(String[] args) {
+        for (int counter = 0 ; counter < 100; counter = counter + 1 ){
+            System.out.println("Hi Deepak Maurya");
+        }
+    }
+}
