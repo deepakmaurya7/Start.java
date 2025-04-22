b29c3d76a8225b013304ba02745aa84f107ff6d3
DEEPAK MAURYA <deepak965147@gmail.com>
2025-04-22 23:07:23 +0530

    Add files via upload

---
+++ Loopsinfinite.java
@@ -0,0 +1,8 @@
+// fex loops note infinate 1 to any tims print hi deepak maurya
+public class Loopsinfinite  { 
+    public static void main(String[] args) {
+        for (int counter = 0 ; ; counter = counter + 1 ){
+            System.out.println("Hi Deepak Maurya");
+        }
+    }
+}
